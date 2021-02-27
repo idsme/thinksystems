abe310f8fafb3e4b1fd13d8487dfc963363a4f45
idsme <idsmesales@gmail.com>
2021-02-27 13:13:23 +0100

    First version of calculator that calculates just in frontend

--- apps/sandbox/src/app/app.module.ts
+++ apps/sandbox/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CalculatorComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
