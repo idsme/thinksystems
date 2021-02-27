44f556e751d174b09367b3a0b681b4135f6996f0
idsme <idsmesales@gmail.com>
2021-02-27 13:11:57 +0100

    Rename Prefix from think-systems to thk

--- apps/sandbox/src/app/app.component.ts
+++ apps/sandbox/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'think-systems-root',
+  selector: 'thk-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
