ea636e3d58c1d11c2776c0cb95efa01abb2f7360
idsme <idsmesales@gmail.com>
2021-03-02 08:46:06 +0100

    Simplified

--- apps/sandbox/src/app/calculator-be/calculator-be.component.ts
+++ apps/sandbox/src/app/calculator-be/calculator-be.component.ts
@@ -12,60 +12,56 @@ import { Observable } from 'rxjs';
 })
 export class CalculatorBeComponent {
 
-  //only one point is permitted
-  decimalPointUsedInInputA = false;
-  decimalPointUsedInInputB = false;
-
-  displayedValue = '0';
+  calculatorDisplay = '0';
 
   inputA = '';
   inputB  = '';
 
+  operationSymbol = '';
   operationType : string = undefined;
   operationSelected = false;
-  operationSymbol = '';
 
   //Injecting the services into the component
   public history$: Observable<Calculation[]>;
 
-  constructor(private restCalculator : CalculatorService) {
-  }
+  constructor(private calculatorService : CalculatorService) {}
+
+  // Where should state be? Service?
+  // Which are helper methods
 
   numberInputSelected(value: string) {
 
-    // if operation type undefined add to input A
-    if(this.operationType == undefined){
-
+    if(this.operationSelected){
         this.inputA = this.inputA.concat(value);
-    }else{
+    } else {
       this.inputB = this.inputB.concat(value);
     }
 
-    this.displayedValue = this.inputA + this.operationSymbol + this.inputB;
+    this.calculatorDisplay = this.inputA + this.operationSymbol + this.inputB;
   }
 
-  operationTypeSelected(operationType: string, symbol: string){
+  operationTypeSelected(operationType: string, symbol: string): void {
     this.operationType = operationType;
     this.operationSymbol = symbol;
     this.operationSelected = true;
   }
 
-  clearCalculator(){
+  clearCalculator(): void {
     this.inputA = '';
     this.inputB = '';
     this.operationType = undefined;
     this.operationSymbol = '';
-    this.displayedValue = '0';
+    this.calculatorDisplay = '0';
     this.operationSelected = false;
   }
 
-  doCalculationInBackend(){
+  doCalculationInBackend() {
     console.log(Number.parseFloat(this.inputA) + " " + Number.parseFloat(this.inputB));
 
-    this.restCalculator.doCalculation(Number.parseFloat(this.inputA), Number.parseFloat(this.inputB), this.operationType).subscribe(
+    this.calculatorService.doCalculation(Number.parseFloat(this.inputA), Number.parseFloat(this.inputB), this.operationType).subscribe(
       res =>{
           // TODO Why display value and operator 1 can this be one thing.
-          this.displayedValue = res.calculation;
+          this.calculatorDisplay = res.calculation;
           this.inputA = '';
           this.inputB = '';
           this.operationSelected = false;
@@ -83,6 +79,6 @@ export class CalculatorBeComponent {
 
 
   showCalculationHistory(): void {
-    this.history$ = this.restCalculator.getCalculationHistory()
+    this.history$ = this.calculatorService.getCalculationHistory()
   }
 }
