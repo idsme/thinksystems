c1e5bf42933ea123cabf35df17e245094301b7e0
idsme <idsmesales@gmail.com>
2022-02-09 14:47:02 +0100

    foundation/stripped out nx welcome page.

--- apps/timeout-react/src/app/app.tsx
+++ apps/timeout-react/src/app/app.tsx
@@ -1,15 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
+//import NxWelcome from './nx-welcome';
 
 import { Route, Link } from 'react-router-dom';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="timeout-react" />
       <div />
-
       {/* START: routes */}
       {/* These routes and navigation have been generated for you */}
       {/* Feel free to move and update them to fit your needs */}
